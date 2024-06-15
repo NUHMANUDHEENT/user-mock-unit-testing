c6da62106a39fc9c0783ec1493ed2e15032489db
nuhmanudheen t <nuhmanudheent@gmail.com>
2024-06-15 11:47:20 +0530

    update users

--- controller/user.go
+++ controller/user.go
@@ -1,77 +1,13 @@
 package controller
 
 import (
-	"log"
 	"net/http"
 	"userPage/database"
 	"userPage/models"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type SignupInput struct {
-	Name     string `json:"name" `
-	Email    string `json:"email" `
-	Password string `json:"password" `
-}
-
-func Signup(c *gin.Context) {
-	var input SignupInput
-	if err := c.ShouldBindJSON(&input); err != nil {
-		log.Println("Failed to bind input:", err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	if input.Name == "" || input.Email == "" || input.Password == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "All fields are required"})
-		return
-	}
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
-	if err != nil {
-		log.Println("Failed to hash password:", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
-		return
-	}
-
-	user := models.Users{Name: input.Name, Email: input.Email, Password: string(hashedPassword)}
-
-	if err := database.DB.Create(&user).Error; err != nil {
-		log.Println("Failed to create user:", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "User created successfully now"})
-}
-
-type SignInInput struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-func SignIn(c *gin.Context) {
-	var singin SignInInput
-	if err := c.ShouldBind(&singin); err != nil {
-		c.JSON(400, gin.H{
-			"error": "Failed to bind data",
-		})
-	}
-	var user models.Users
-	if err := database.DB.Where("email=?", singin.Email).First(&user).Error; err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"error": "Invalid email or password",
-		})
-	}
-
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(singin.Password)); err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
-}
 func UserList(c *gin.Context) {
 	var users []models.Users
 	if err := database.DB.Find(&users).Error; err != nil {
