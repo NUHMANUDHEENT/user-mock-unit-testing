4d503cbb08238b8371a0b7b31b68a4d906369f00
nuhmanudheen t <nuhmanudheent@gmail.com>
2024-06-18 13:56:36 +0530

    create admin login

--- models/model.go
+++ models/model.go
@@ -2,9 +2,15 @@ package models
 
 import "gorm.io/gorm"
 
-type Users struct {	
+type Users struct {
 	gorm.Model
 	Name     string `gorm:"not null"`
 	Email    string `gorm:"not null;unique"`
 	Password string `gorm:"not null"`
+}
+
+type Admin struct {
+	Name     string
+	Email    string
+	Password string
 }
